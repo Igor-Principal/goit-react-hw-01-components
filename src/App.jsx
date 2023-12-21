60ca6d3f1293aeb561eb725c817ca6fbf6b56eba
Igor-Principal <igorprincipal@gmail.com>
2023-12-21 23:49:56 +0200

    Created third task

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import User from './components/User/User';
 import user from './data/user.json';
 import Statistics from 'components/Statistics/Statistics';
 import data from './data/data.json'
+import FriendList from 'components/FriendList/FriendList';
+import friends from './data/friends.json'
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import transactions from "./data/transactions"
 
 
 export const App = () => {
@@ -9,6 +13,8 @@ export const App = () => {
     <main>
       <User items={user} />
       <Statistics title="UPLOAD STATS" items={data} />
+      <FriendList items={friends} />
+      <TransactionHistory items={transactions} />
     </main>
   );
 };
